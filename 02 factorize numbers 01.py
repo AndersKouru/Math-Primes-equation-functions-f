ac6ea353283c6ee29d3b1db6cc2e0cc95c173cd5
Anders Kouru <anderskouru@gmail.com>
2018-09-27 01:12:49 +0200

    Add files via upload

---
+++ 02 factorize numbers 01.py
@@ -0,0 +1,87 @@
+# Factorizes numbers
+# Anders Kouru 30 juni 2014
+
+
+import math
+
+def introFunction():
+        print("This function Factors numbers into Primes!")
+
+def floor(num):
+        tal = float(num)
+        ital = int(num)
+        numrest = tal - ital
+        tal -= numrest
+        return tal
+
+def primeFactor(prim_tal):
+        divisor = 2.0
+        prim_spar = prim_tal
+        end = pow(prim_spar,0.5)
+        end_spar = 1
+        end_spar = int(end)
+        prim_spar = float(prim_spar)
+        prim = 1                # We asume the number is prime!
+        antal_div = 0
+        rest  = 1.0
+        test  = 1.0
+        potens = 1.0
+        potens = 0
+        print("factorizing !         ")
+        print("")
+        print("Number ",prim_spar," factorizes! ")
+        print("")
+        prim_tal = prim_spar
+        while divisor <= prim_tal and prim_tal > 2:  # try end in stead of prim_tal
+            rest = prim_tal/divisor
+            test = floor(rest)
+            while rest == test and prim_tal >= divisor:# try end ii stead of divisor
+                prim_tal = rest
+                rest = (prim_tal/divisor) # Let us see how many times
+                test = floor(rest)        # we can divide with divisor
+                potens += 1
+                prim += 1
+            if potens > 0:
+                print(divisor, end='')
+                if potens > 1:
+                    antal_div += 1
+                    print(" ^",potens, end='')
+                potens = 0
+                if prim_tal > divisor:
+                    print("  *  ", end='')
+            divisor += 1
+        if prim == 1 or prim == 2 and antal_div <= 1:
+            print(" is Prime!  ")           
+        return 0
+
+#prim_spar = 2
+prim_tal  = 2
+def main():
+    forts = 1
+    while forts != 0:
+        introFunction()
+        inputFunction()
+        print("")
+        print("")
+        forts = input("continue = 1,  end = 0                       ")
+        if forts == '0':
+                break
+    return 0
+
+pp = '2'
+def inputFunction():
+    print("inputFunction ")     
+    print("Which number should be factored?                    ")
+    prim_tal = input("It should be 2 or larger.                    ")
+    prim_tal = int(prim_tal)
+    while prim_tal < 2:
+        print("      ")
+        prim_tal = input("It should be 2 or larger.                    ")
+        prim_tal = int(prim_tal)
+        print("prim_tal i inputFunction = ",prim_tal)
+    primeFactor(prim_tal)
+    return 0
+
+main()
+
+
