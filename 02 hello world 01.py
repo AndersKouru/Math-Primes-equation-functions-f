e4f392071a203de4ad32346edc47e019b1149999
Anders Kouru <anderskouru@gmail.com>
2018-09-27 03:34:19 +0200

    Add files via upload

---
+++ 02 hello world 01.py
@@ -0,0 +1,18 @@
+# Hello World
+# 10 mars 2017,  Anders Kouru
+
+
+
+def main():
+    number = int(1)                   
+    while number != 0:
+        print(" Hello World!")  
+        print(" Continue = 1, Zero ends!          ",end= ' ')
+        number = input("")
+        number = int(number)
+        print('')    
+    return 0
+
+main()
+
+
